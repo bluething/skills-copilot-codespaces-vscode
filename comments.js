02f1b761d5eb426c9caf441b2b716ae18a5c149a
Habib Ali Machpud <habib.machpud@gmail.com>
2025-03-14 03:54:10 +0000

    Copilot third commit, make it run

--- comments.js
+++ comments.js
@@ -1,22 +1,61 @@
 // create a web server that listens on port 3000
-// when a request is made to the server, send the contents of the comments.json file to the client
+// The server should respond to a GET request to /comments with an array of comments
+// The server should respond to a POST request to /comments with a new comment added to the array
+// The server should respond with a 404 to any other requests
+// The server should respond with a 500 if there is an error
+// The server should respond with a 200 if the request was successful
+// The server should respond with a 201 if the comment was created successfully
 
-// Load the http module to create an http server.
+// Comments array
+var comments = [];
+
+// Require the http module
 var http = require('http');
-var fs = require('fs');
+// Require the url module
+var url = require('url');
+// Require the querystring module
+var querystring = require('querystring');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "application/json"});
+// Create the server
+var server = http.createServer(function(req, res) {
+  // Get the URL
+  var urlParts = url.parse(req.url);
+  var path = urlParts.pathname;
+  var query = querystring.parse(urlParts.query);
 
-  fs.readFile("comments.json", "utf8", function(err, data) {
-    if (err) {
-      console.log("Error reading file");
+  // Check if the path is comments
+  if (path === '/comments') {
+    if (req.method === 'GET') {
+      // Send back the comments array
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(comments));
+    } else if (req.method === 'POST') {
+      // Get the data from the request
+      var data = '';
+      req.on('data', function(chunk) {
+        data += chunk;
+      });
+      req.on('end', function() {
+        // Parse the data
+        var comment = JSON.parse(data);
+        // Add the comment to the comments array
+        comments.push(comment);
+        // Send back a 201
+        res.writeHead(201);
+        res.end();
+      });
     } else {
-      response.write(data);
-      response.end();
+      // Send back a 404
+      res.writeHead(404);
+      res.end();
     }
-  });
+  } else {
+    // Send back a 404
+    res.writeHead(404);
+    res.end();
+  }
 });
 
-// Listen on port 3000, IP defaults to
+// Listen on port 3000
+server.listen(3000);
+console.log('Server listening on port 3000');
